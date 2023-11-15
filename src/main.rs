ef6d969f0fa3c2591e0ee176f051ddda4a303d26
anthony-y <anthonyybayn@gmail.com>
2023-11-15 18:42:35 +0000

    Todos path can now be specified as an env var; doc comments added; cleanup, fixes.

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,28 @@
+mod weather;
 use std::fs;
 use std::env;
-use std::io::{self, Read, BufRead, BufReader, ErrorKind, Error};
-use reqwest;
-use serde::{Serialize, Deserialize};
+use std::io::{self, Read, BufRead, BufReader};
 use colored::Colorize;
 use tokio;
 
-const TODOS_PATH: &str = "/home/ant/.local/share/rwelcome/todos";
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct LocationInfo {
-    name: String,
-    region: String,
-    country: String,
-    lat: f64,
-    lon: f64,
-    tz_id: String,
-    localtime_epoch: i64,
-    localtime: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ConditionInfo {
-    text: String,
-    icon: String,
-    code: u16,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct CurrentWeatherInfo {
-    last_updated_epoch: i64,
-    last_updated: String,
-    temp_c: f64,
-    temp_f: f64,
-    is_day: u8,
-    condition: ConditionInfo,
-    wind_mph: f64,
-    wind_kph: f64,
-    wind_degree: u16,
-    wind_dir: String,
-    pressure_mb: f64,
-    pressure_in: f64,
-    precip_mm: f64,
-    precip_in: f64,
-    humidity: u8,
-    cloud: u8,
-    feelslike_c: f64,
-    feelslike_f: f64,
-    vis_km: f64,
-    vis_miles: f64,
-    uv: f64,
-    gust_mph: f64,
-    gust_kph: f64,
-}
-
-#[derive(Deserialize, Debug)]
-struct WeatherResponse {
-    location: LocationInfo,
-    current: CurrentWeatherInfo,
-}
-
-async fn acquire_weather() -> Result<WeatherResponse, reqwest::Error> {
-
-    let key = env::var("RWELCOME_WEATHER_API_KEY").expect("rwelcome: error: please bind an API key to the RWELCOME_WEATHER_API_KEY environment variable to display weather information.");
-
-    let location = env::var("RWELCOME_WEATHER_LOCATION").unwrap_or_else(|_| "Brighton".to_string());
-
-    let url = format!(
-        "https://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no",
-        key,
-        location
-    );
-
-    let res = reqwest::get(url).await?;
-
-    let weather_res = res.json::<WeatherResponse>().await?;
-
-    Ok(weather_res)
-}
-
+/// Neatly format a list of todos to stdout.
 fn show_todos(todos: Vec<String>) {
-
     if todos.is_empty() {
-        println!("{}: all done!", "Todos".bright_blue());
+        println!("{}: none!", "Todos".bright_blue());
         return;
     }
-
     println!("{}:", "Todos".bright_blue());
     for (index, todo) in todos.iter().enumerate() {
         println!("  {}. {}", index + 1, todo);
     }
 }
 
-fn acquire_todos() -> io::Result<Vec<String>> {
-    let file = fs::File::open(TODOS_PATH)?;
-    let reader = BufReader::new(file);
+/// Acquire todos from the filesystem at `TODOS_PATH`.
+fn acquire_todos(todos_path: String) -> io::Result<Vec<String>> {
+    let file = fs::File::open(todos_path)?;
+    let reader = io::BufReader::new(file);
     let mut todos = Vec::<String>::new();
+
     for line in reader.lines() {
         let line = line?;
         if line.is_empty() || !line.starts_with('#') {
@@ -104,57 +31,65 @@ fn acquire_todos() -> io::Result<Vec<String>> {
         if line.len() == 1 {
             continue;
         }
-
         // We are basically just skipping the # (and space after it, if there is one.)
         // If you add more than one space, that will not be excluded from the todo text.
         let offset = if line.chars().nth(1).unwrap() == ' ' { 2 } else { 1 };
-
         todos.push(line[offset..].to_string());
     }
 
     Ok(todos)
 }
 
+/// Acquire the current user by looking at the LOGNAME or USER environment variables.
 fn acquire_current_user() -> Option<String> {
     env::var("LOGNAME")
         .or_else(|_| env::var("USER")).ok()
 }
 
+/// Acquire the system's hostname from the filesystem.
+/// More specifically, from /proc/sys/kernel/hostname.
 fn acquire_hostname() -> std::io::Result<String> {
     fs::read_to_string("/proc/sys/kernel/hostname")
         .map(|s| s.trim().to_string())
 }
 
+/// Acquire the CPU temperature from the filesystem.
+/// More specifically, from /sys/class/hwmon/hwmon1/temp2_input (by default).
+/// If a value is bound to the environment variable RWELCOME_CPU_TEMP, it will look there instead.
 fn acquire_cpu_temperature() -> io::Result<f64> {
-    
-    const PATH: &str = "/sys/class/hwmon/hwmon1/temp2_input";
-    let contents = fs::read_to_string(PATH)?;
+    let path = env::var("RWELCOME_CPU_TEMP_PATH")
+        .unwrap_or("/sys/class/hwmon/hwmon1/temp2_input".to_string());
+
+    let contents = fs::read_to_string(path)?;
 
     let temp_millidegrees: i32 = contents
                                 .trim()
                                 .parse()
                                 .map_err(|_| {
-                                    Error::new(
-                                        ErrorKind::InvalidData,
-                                        "Invalid temperature data"
+                                    io::Error::new(
+                                        io::ErrorKind::InvalidData,
+                                        "invalid temperature data"
                                     )
                                 })?;
 
     Ok(temp_millidegrees as f64 / 1000.0)
 }
 
+/// Acquires the kernel version from the filesystem.
+/// More specifically, from /proc/version.
 fn acquire_kernel_version() -> io::Result<String> {
     let contents = fs::read_to_string("/proc/version")?;
     let version_info = contents.split_whitespace()
         .nth(2) // the kernel version typically appears as the third word in /proc/version
         .ok_or(io::Error::new(
             io::ErrorKind::InvalidData,
-            "Invalid kernel version data")
+            "invalid kernel version")
         )?
         .to_owned();
     Ok(version_info)
 }
 
+/// Attempts to acquire the current system uptime from the filesystem.
 fn acquire_uptime() -> io::Result<(u64, u64)> {
     let mut file       = fs::File::open("/proc/uptime")?;
     let mut contents = String::new();
@@ -162,7 +97,7 @@ fn acquire_uptime() -> io::Result<(u64, u64)> {
 
     let uptime_str = contents.split_whitespace().next().ok_or(io::Error::new(
         io::ErrorKind::InvalidData,
-        "Invalid uptime data",
+        "invalid uptime data",
     ))?.trim();
 
     let uptime_seconds = uptime_str.parse::<f32>().map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
@@ -173,10 +108,12 @@ fn acquire_uptime() -> io::Result<(u64, u64)> {
     Ok((hours, minutes))
 }
 
+/// Attempts to parse a memory value given as a string value into a number.
+/// This is a util function used exclusively by the below `acquire_memory_info()`.
 fn parse_memory_value(value: &str) -> io::Result<u64> {
     let value = value.split_whitespace().next().ok_or(io::Error::new(
         io::ErrorKind::InvalidData,
-        "Invalid memory data",
+        "invalid memory data",
     ))?;
     value
         .parse()
@@ -188,6 +125,8 @@ fn parse_memory_value(value: &str) -> io::Result<u64> {
         })
 }
 
+/// Attempts to acquire, from the filesystem, the used and total memory
+/// on the system at the moment. More specifically, from /proc/meminfo.
 fn acquire_memory_info() -> io::Result<(u64, u64)> {
     let file = fs::File::open("/proc/meminfo")?;
     let reader = BufReader::new(file);
@@ -211,69 +150,142 @@ fn acquire_memory_info() -> io::Result<(u64, u64)> {
     Ok((used_memory, total_memory))
 }
 
-fn edit_todos(wants_editor: bool) {
+/// Displays an interface allowing the user to edit the todo list.
+/// If `wants_editor` is true, it will attempt to open an instance of
+/// an appropriate text editor with the todos file loaded.
+/// Otherwise, it will render an interactive TUI allowing the addition
+/// and removal of todo list items.
+/// If anything goes wrong, it will return an Err containing an error
+/// message string that the caller can display to the user.
+fn edit_todos(wants_editor: bool, todos_path: String) -> Result<(), &'static str> {
+
     if wants_editor {
         let editor = env::var("EDITOR")
                             .unwrap_or_else(|_| "vi".to_string());
 
         let status = std::process::Command::new(editor)
-            .arg(TODOS_PATH)
+            .arg(todos_path)
             .status()
-            .expect("Failed to execute editor");
-
+            .expect("rwelcome: error: failed to execute editor");
+        
         if !status.success() {
-            panic!("Editor exited with non-zero status code");
+            return Err("rwelcome: error: editor exited with non-zero status code");
+        }
+
+        return Ok(());
+    }
+
+    draw_line(12);
+
+    let current_todos = match acquire_todos(todos_path) {
+        Ok(todos) => todos,
+        Err(err) => panic!("rwelcome: error: couldn't acquire todo list: {}", err),
+    };
+
+    show_todos(current_todos);
+
+    loop {
+        let mut selection = String::new();
+        match io::stdin().read_line(&mut selection) {
+            Ok(_) => (),
+            Err(_) => return Err("rwelcome: error: there was a problem reading your selection")
         }
     }
 }
 
-#[tokio::main]
-async fn main() {
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() > 1 {
-        let given_arg = &args[1];
-        if given_arg == "edit" {
-            let wants_editor = args.len() > 2 && args[2] == "--editor";
-            edit_todos(wants_editor);
-        }
-    }
-
-    let username = acquire_current_user().unwrap_or_else(|| "unknown".to_string());
-    let hostname = acquire_hostname().unwrap_or_else(|_| "unknown".to_string());
-    println!("{}@{}", username.bright_red(), hostname);
+// Send _n_ hyphens to stdout, where _n_ equals `length`.
+fn draw_line(length: usize) {
     let mut i = 0;
     loop {
-        if username.len() + hostname.len() == i {
+        if length == i {
             println!();
             break;
         }
         print!("-");
         i += 1;
     }
+}
+
+#[tokio::main]
+async fn main() {
+
+    // Make the request to the weather service at the beginning, so that
+    // it has time to get a response before we go to render everything.
+    // This should mean there's less of a pause when printing to the screen.
+    //
+    // (In theory, this could also be done when acquiring info from the filesystem,
+    // such as uptime and kernel version.)
+    //
+
+    let wants_weather = match env::var("RWELCOME_WEATHER") {
+        Ok(val) => val.parse::<bool>().unwrap_or(true),
+        Err(err) => if err == env::VarError::NotPresent { true } else { false },
+    };
+
+    let maybe_weather_response = 
+        if wants_weather { Some(weather::acquire().await) }
+        else { None };
+
+    let todos_path = env::var("RWELCOME_TODOS_PATH")
+        .unwrap_or("/home/ant/.local/share/rwelcome/todos".to_string());
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let given_arg = &args[1];
+        if given_arg == "edit" {
+            let wants_editor = args.len() > 2 && args[2] == "--editor";
+            match edit_todos(wants_editor, todos_path.clone()) {
+                Ok(_) => (),
+                Err(msg) => {
+                    eprintln!("{}", msg)
+                }
+            }
+        }
+    }
+    println!();
+    let username = acquire_current_user().unwrap_or_else(|| "unknown".to_string());
+    let hostname = acquire_hostname().unwrap_or_else(|_| "unknown".to_string());
+    println!("{}@{}", username.bright_red(), hostname);
+    draw_line(username.len() + hostname.len() + 1);
     match acquire_uptime() {
         Ok((hours, minutes)) => println!("{}: {}h {}m", "Uptime".bright_blue(), hours, minutes),
-        Err(err) => panic!("{}: {}", "Uptime".red(), err)
+        Err(err) => eprintln!("{}: {}", "Uptime".red(), err)
     }
     match acquire_memory_info() {
         Ok((used, total)) => println!("{}: {} MiB / {} MiB", "Memory".bright_blue(), used / 1000, total / 1000),
-        Err(err) => panic!("{}: {}", "Memory".red(), err),
+        Err(err) => eprintln!("{}: {}", "Memory".red(), err),
     }
     match acquire_kernel_version() {
-        Ok(version) => println!("{}: {}", "Kernel".bright_blue(), version),
-        Err(err) => panic!("{}: {}", "Kernel".red(), err)
+        Ok(version) => println!("{}: Linux {}", "Kernel".bright_blue(), version),
+        Err(err) => eprintln!("{}: {}", "Kernel".red(), err)
     }
     match acquire_cpu_temperature() {
-        Ok(temp) => println!("{}: {:.1}°C", "CPU Temp".bright_blue(), temp),
-        Err(err) => println!("{}: {}", "CPU Temp".red(), err)
+        Ok(temp) => println!("{}: {:.1}°C", "CPU temp".bright_blue(), temp),
+        Err(err) => eprintln!("{}: {}", "CPU temp".red(), err)
     }
-    match acquire_weather().await {
-        Ok(weather) => println!("{}: {}  It's {}°C and {} in {}", "Weather".bright_blue(), "☁", weather.current.temp_c, weather.current.condition.text.to_lowercase(), weather.location.name),
-        Err(err) => println!("{}: {}", "Weather".red(), err),
+    if let Some(weather_response) = maybe_weather_response {
+        match weather_response {
+            Ok(weather) => {
+                let the_condition = weather.current.condition.text.to_lowercase();
+                let emoji = if the_condition == "cloudy" { "☁️" }
+                                else if the_condition.contains("sunny") { "🌤️" }
+                                else if the_condition.contains("rain") { "🌧️" }
+                                else { "🌥️" };
+                println!(
+                    "{}: {}°C and {} in {} {}",
+                    "Weather".bright_blue(),
+                    weather.current.temp_c,
+                    the_condition,
+                    weather.location.name,
+                    emoji,
+                );
+            },
+            Err(err) => eprintln!("{}: {}", "Weather".red(), err),
+        }
     }
-    match acquire_todos() {
+    match acquire_todos(todos_path) {
         Ok(todos) => show_todos(todos),
-        Err(err) => println!("{}: {}", "Todos".red(), err),
+        Err(err) => eprintln!("{}: {}", "Todos".red(), err),
     };
     println!();
 }
---
+++ src/weather.rs
@@ -0,0 +1,72 @@
+use reqwest;
+use serde::{Serialize, Deserialize};
+use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LocationInfo {
+    pub name: String,
+    pub region: String,
+    pub country: String,
+    pub lat: f64,
+    pub lon: f64,
+    pub tz_id: String,
+    pub localtime_epoch: i64,
+    pub localtime: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ConditionInfo {
+    pub text: String,
+    pub icon: String,
+    pub code: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CurrentWeatherInfo {
+    pub last_updated_epoch: i64,
+    pub last_updated: String,
+    pub temp_c: f64,
+    pub temp_f: f64,
+    pub is_day: u8,
+    pub condition: ConditionInfo,
+    pub wind_mph: f64,
+    pub wind_kph: f64,
+    pub wind_degree: u16,
+    pub wind_dir: String,
+    pub pressure_mb: f64,
+    pub pressure_in: f64,
+    pub precip_mm: f64,
+    pub precip_in: f64,
+    pub humidity: u8,
+    pub cloud: u8,
+    pub feelslike_c: f64,
+    pub feelslike_f: f64,
+    pub vis_km: f64,
+    pub vis_miles: f64,
+    pub uv: f64,
+    pub gust_mph: f64,
+    pub gust_kph: f64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct WeatherResponse {
+    pub location: LocationInfo,
+    pub current: CurrentWeatherInfo,
+}
+
+pub async fn acquire() -> reqwest::Result<WeatherResponse> {
+    let key = env::var("RWELCOME_WEATHER_API_KEY")
+                     .expect("rwelcome: error: please bind an API key to the RWELCOME_WEATHER_API_KEY environment variable to display weather information.");
+    let location = env::var("RWELCOME_WEATHER_LOCATION")
+                          .unwrap_or_else(|_| "Brighton".to_string());
+
+    let url = format!(
+        "https://api.weatherapi.com/v1/current.json?key={}&q={}&aqi=no",
+        key,
+        location
+    );
+    let res = reqwest::get(url).await?;
+
+    let weather_res: WeatherResponse = res.json().await?;
+    Ok(weather_res)
+}
